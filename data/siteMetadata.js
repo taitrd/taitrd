8fd42ed3a7056e356ed7cad956ebded7d85c9fbb
taitrd <tai.tran@texo.design>
2024-05-01 13:59:05 +0700

    upd desc

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Tai\'s profile',
   author: 'taitrd',
   headerTitle: 'taitrd',
-  description: 'Tai is a Software Engineer in Vietnam. He works in weekdays and he love to developing his site projects in nights and weekends. That would be amazing when he found a good job.',
+  description: 'Tai is a Software Engineer in Vietnam. He works in weekdays and he love to developing his side projects in nights and weekends. That would be amazing when he has a good job with potentialities and professional process.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
