67c75372bec89b09f87452eb6e1558bdbd4a2289
taitrd <tai.tran@texo.design>
2024-11-29 21:45:36 +0700

    fix ui about layout

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 import SocialIcon from '@components/social-icons'
 import Image from '@components/Image'
+import siteMetadata from '@/data/siteMetadata'
 
 interface Props {
   children: ReactNode
@@ -15,7 +16,7 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            About me
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
@@ -33,10 +34,10 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
+              <SocialIcon kind="twitterx" href={siteMetadata.twitter} />
+              <SocialIcon kind="mail" href={`mailto:${email}`} />
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 xl:col-span-2 dark:text-gray-200 text-gray-600">
