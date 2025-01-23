8fb04946f4fc0b38e6af147eecbfcb379c4caa6a
taitrd <trevortran61@gmail.com>
2025-01-23 10:54:42 +0700

    fix/400

--- app/api/contributions/route.ts
+++ app/api/contributions/route.ts
@@ -6,14 +6,6 @@ export const GET = async (req: Request) => {
   const github_contributions = await collectGithubContributions();
   const gitlab_contributions = await collectGitlabContributions();
   const bitbucket_contributions = await collectBitbucketContributions();
-  if (!github_contributions) {
-    return Response.json(
-      {
-        message: "Not found contributions",
-      },
-      { status: 400 }
-    );
-  }
   return Response.json(
     {
       github_contributions,
