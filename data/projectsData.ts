b6e8918edd734ce666b1ac47b240727ad5a38ee2
taitrd <trevortran61@gmail.com>
2025-07-30 12:33:37 +0700

    added dtui project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,10 +8,17 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: "Tailwind CSS Component Library",
+    description: `A bundle of templates built with Tailwind CSS and shadcn/ui, generated with AI assistance.`,
+    imgSrc: "/imgs/dtui.taitrd.com.jpeg",
+    tags: ["Tailwind CSS", "Shadcn/ui"],
+    href: "https://dtui.taitrd.com/components?industry=heroes#concentric-gradient-hero",
+  },
+  {
     title: "Trucking Service Landing Page",
     description: `A landing page template for a trucking service featuring a hero video background.`,
     imgSrc: "/imgs/trucking.jpeg",
-    tags: ["HTML", "TailwindCSS", "JavaScript"],
+    tags: ["HTML", "Tailwind CSS", "JavaScript"],
     href: "https://i4t-new-landing-page.vercel.app/",
   },
   {
@@ -23,7 +30,7 @@ const projectsData: Project[] = [
   },
   {
     title: "Dtite Custom Website",
-    description: `A landing page for a custom website featuring a user-friendly admin panel, headless CMS, and the latest technologies.`,
+    description: `A landing page for a custom website featuring a user-friendly admin panel, headless CMS, and modern technologies.`,
     imgSrc: "/imgs/dtite.png",
     tags: ["Laravel", "Next.js", "React.js"],
     href: "https://dtite.taitrd.com/",
@@ -35,13 +42,13 @@ const projectsData: Project[] = [
     tags: ["Next.js", "React.js", "TailwindCSS"],
     href: "https://pricingportal.taitrd.com/",
   },
-  {
-    title: "E-commerce Platform",
-    description: `R&D and development of over 10 eCommerce website templates in one data center, featuring product search, cart & checkout, payments, shipping, and more.`,
-    imgSrc: "/imgs/buysimvietnam.au.jpeg",
-    tags: ["Laravel", "Next.js", "React.js"],
-    href: "https://buysimvietnam.com.au/",
-  },
+  // {
+  //   title: "E-commerce Platform",
+  //   description: `R&D and development of over 10 eCommerce website templates in one data center, featuring product search, cart & checkout, payments, shipping, and more.`,
+  //   imgSrc: "/imgs/buysimvietnam.au.jpeg",
+  //   tags: ["Laravel", "Next.js", "React.js"],
+  //   href: "https://buysimvietnam.com.au/",
+  // },
   {
     title: "OpenAPI 3.0 Client Tool", 
     description: `Building an OpenAPI 3.0 client tool with AI support.`,
