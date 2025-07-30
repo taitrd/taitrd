9816a2ce0eb32ad3ebe4eefe9008ce69056f5db9
taitrd <trevortran61@gmail.com>
2025-07-30 12:32:22 +0700

    api route to action

--- hooks/use-contributions.ts
+++ hooks/use-contributions.ts
@@ -1,8 +1,29 @@
-import { fetcher } from "@/lib/fetcher";
-import { ServiceContributions } from '@/lib/types';
-import useSWR from "swr";
+import { getContributions } from "@/lib/actions/contribution";
+import { ServiceContributions } from "@/lib/types";
+import { useEffect, useState } from "react";
 
 const useContributions = () => {
-  return useSWR<ServiceContributions>("/api/contributions", fetcher);
+  const [data, setData] = useState<ServiceContributions>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const fetchContributions = async () => {
+    try {
+      const {
+        success,
+        data: contributions,
+        message,
+      } = await getContributions();
+      if (!success || !contributions) {
+        throw new Error(message);
+      }
+      setData(contributions as ServiceContributions);
+      setIsLoading(false);
+    } catch {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchContributions();
+  }, []);
+  return { data, isLoading };
 };
 export default useContributions;
