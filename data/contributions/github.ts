4b7466b4f32aae1f2f84ab85aaf5a58a0645eb51
taitrd <trevortran61@gmail.com>
2026-03-19 03:48:27 +0000

    fix: change git event function

--- data/contributions/github.ts
+++ data/contributions/github.ts
@@ -2,14 +2,15 @@ import { AWS_ENABLE_SYNC } from "../../lib/constants/aws";
 import putContributions from "./put-contributions";
 
 import { getGithubContributions } from "./get-contributions";
-import { getRepositoryEventData } from "./github/get-repository-events";
+// import { getRepositoryEventData } from "./github/get-repository-events";
+import { getEventData } from "./github/get-events";
 
 export const collectGithubContributions = async () => {
   /**
    * TODO: change Event API when needed
    * Get mores event data with repo and events API
    */
-  const eventData = await getRepositoryEventData();
+  const eventData = await getEventData();
   let contributionsItem = await getGithubContributions();
 
   if (AWS_ENABLE_SYNC) {
