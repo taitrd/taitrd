5f02db5573cbcae881b3e2929265b7b5c6ac563b
taitrd <trevortran61@gmail.com>
2026-04-23 06:53:53 +0700

    ref: remove cache fetching github data

--- data/contributions/github/get-events.ts
+++ data/contributions/github/get-events.ts
@@ -1,26 +1,20 @@
 import EventAPI from "@/lib/github-apis/event";
 import { EventGroup } from "@/lib/types/github-api.type";
-import { unstable_cache } from "next/cache";
 import { eventsGroupingReducer } from "./get-events-legacy";
-import { CacheTag } from "@/lib/enums/cach-tag";
 
 /**
  * New API for get event data
  */
-export const getEventData = unstable_cache(
-  async () => {
-    const { allEvents } =
-      await EventAPI().getEventsForAuthenticatedUserByRequest();
-    const groupedEvents = allEvents.reduce<EventGroup[]>(
-      eventsGroupingReducer,
-      [],
-    );
-    // console.log("events", groupedEvents);
-    return {
-      allEvents,
-      groupedEvents,
-    };
-  },
-  ["github_list_events", "authenticated", "rest_api_request"],
-  { revalidate: 60, tags: [CacheTag.Github] },
-);
+export const getEventData = async () => {
+  const { allEvents } =
+    await EventAPI().getEventsForAuthenticatedUserByRequest();
+  const groupedEvents = allEvents.reduce<EventGroup[]>(
+    eventsGroupingReducer,
+    [],
+  );
+  // console.log("events", groupedEvents);
+  return {
+    allEvents,
+    groupedEvents,
+  };
+};
--- data/contributions/github/get-repository-events.ts
+++ data/contributions/github/get-repository-events.ts
@@ -1,23 +1,17 @@
 import EventAPI from "@/lib/github-apis/event";
 import { EventGroup } from "@/lib/types/github-api.type";
-import { unstable_cache } from "next/cache";
 import { eventsGroupingReducer } from "./get-events-legacy";
-import { CacheTag } from "@/lib/enums/cach-tag";
 import { getRepositories } from "./get-repositories";
-export const getRepositoryEventData = unstable_cache(
-  async () => {
-    const { repoParams } = await getRepositories();
-    console.table(repoParams);
-    const { allEvents } = await EventAPI().getEventsByRepos(repoParams, 1, 5);
-    const groupedEvents = allEvents.reduce<EventGroup[]>(
-      eventsGroupingReducer,
-      [],
-    );
-    return {
-      allEvents,
-      groupedEvents,
-    };
-  },
-  ["github_list_events", "all_repos"],
-  { revalidate: 3600, tags: [CacheTag.Github] },
-);
+export const getRepositoryEventData = async () => {
+  const { repoParams } = await getRepositories();
+  console.table(repoParams);
+  const { allEvents } = await EventAPI().getEventsByRepos(repoParams, 1, 5);
+  const groupedEvents = allEvents.reduce<EventGroup[]>(
+    eventsGroupingReducer,
+    [],
+  );
+  return {
+    allEvents,
+    groupedEvents,
+  };
+};
